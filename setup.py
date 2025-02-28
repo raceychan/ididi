f91fe158b52610572531e0f465c6940bffb0e0ee
raceychan <raceychan@gmail.com>
2025-02-28 20:17:41 +0800

    chores: version

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from Cython.Build import cythonize
 from setuptools import setup
 
+from ididi import VERSION
+
 extensions = ["ididi/graph.pyx", "ididi/_node.py", "ididi/_ds.py"]
 setup(
     name="ididi",
+    version=VERSION,
     ext_modules=cythonize(extensions),
 )
