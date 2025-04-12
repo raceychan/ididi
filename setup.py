0fbfbbb68dd40d8db8a8501bfdb3d6d67b278120
raceychan <raceychan@gmail.com>
2025-04-13 04:51:00 +0800

    ci: upload test-pypi

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 
 __version__ = "1.5.4"
 VERSION = __version__
+
+#TODO: write version in a file
+
 extensions = ["ididi/graph.pyx", "ididi/_node.py", "ididi/_ds.py"]
 
 setup(
