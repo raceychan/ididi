161392c8c48f0a1ecde4cafbdeafdfb393fbbb84
raceychan <raceychan@gmail.com>
2025-12-30 21:33:24 +0400

    Release version 1.8.4

--- ididi/__init__.py
+++ ididi/__init__.py
@@ -33,7 +33,7 @@ from .interfaces import INode as INode
 from .interfaces import Resource as Resource
 from .utils.param_utils import is_provided as is_provided
 
-VERSION="1.8.3"
+VERSION="1.8.4"
 
 try:
     import graphviz as graphviz  # type: ignore
